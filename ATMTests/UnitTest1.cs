43cb8f8cf67b947a5aa5e058ab9261e1f3b5952c
Jon Rice <revyolution1120@gmail.com>
2019-06-12 17:20:25 -0700

    Tests and most exceptions covered.

--- ATMTests/UnitTest1.cs
+++ ATMTests/UnitTest1.cs
@@ -6,12 +6,6 @@ namespace ATMTests
 {
     public class UnitTest1
     {
-    [Fact]
-    public void canViewBalance()
-        {
-            Assert.Equal(1000, Program.ViewBalance());
-        }
-
     [Theory]
     [InlineData(900, 100)]
     [InlineData(750, 250)]
@@ -20,12 +14,44 @@ namespace ATMTests
 
         public void canWithdraw(int expected, int withdrawn)
         {
-            Assert.Equal(expected, Program.Withdrawal(withdrawn));
+            Program.balance = 1000;
+            Assert.Equal(expected, Program.Withdraw(withdrawn, Program.balance));
         }
-    [Fact]
-    public void canDeposit()
+
+        [Theory]
+        [InlineData(1100, 100)]
+        [InlineData(1250, 250)]
+        [InlineData(2000, 1000)]
+        [InlineData(1500, 500)]
+
+        public void canDeposit(int expected, int deposit)
         {
-            Assert.Equal(50, Program.Deposit());
+            Program.balance = 1000;
+            Assert.Equal(expected, Program.Deposit(deposit, Program.balance));
+        }
+
+        [Theory]
+        [InlineData(3, -10)]
+        [InlineData(3, -100)]
+        [InlineData(3, -5)]
+        [InlineData(3, -6)]
+
+        public void cannotDepositNegative(int expected, int deposit)
+        {
+            Program.balance = 1000;
+            Assert.Equal(expected, Program.Deposit(deposit, Program.balance));
+        }
+
+        [Theory]
+        [InlineData(2, 10)]
+        [InlineData(2, 100)]
+        [InlineData(2, 5)]
+        [InlineData(2, 6)]
+
+        public void cannotOverWithdraw(int expected, int withdrawn)
+        {
+            Program.balance = 0;
+            Assert.Equal(expected, Program.Withdraw(withdrawn, Program.balance));
         }
     }
 }
